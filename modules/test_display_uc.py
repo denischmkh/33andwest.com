b698be18e2021a0014b0f14fd39ecbd9483f7c55
denischmkh <avraam.l2004@gmail.com>
2025-08-05 18:35:31 +0300

    fix

---
+++ modules/test_display_uc.py
@@ -0,0 +1,21 @@
+from pyvirtualdisplay import Display
+import os
+import undetected_chromedriver as uc
+
+display = Display(visible=False, size=(1280, 720))
+display.start()
+print("DISPLAY:", os.environ.get("DISPLAY"))  # должно быть НЕ ":0"
+
+try:
+    options = uc.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+
+    driver = uc.Chrome(options=options)
+    driver.get("https://example.com")
+    print("Title:", driver.title)
+    driver.quit()
+
+finally:
+    display.stop()
