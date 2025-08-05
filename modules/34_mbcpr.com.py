a2901156b5afcc3f49d2c796868f44e19f583dc9
denischmkh <avraam.l2004@gmail.com>
2025-08-05 12:39:45 +0300

    fix

--- modules/34_mbcpr.com.py
+++ modules/34_mbcpr.com.py
@@ -21,13 +21,13 @@ from parser_app.models import Artist
 today = timezone.now().date()
 
 options = uc.ChromeOptions()
-options.add_argument("--headless=new")  # обязательно!
+# options.add_argument("--headless")  # обязательно!
 options.add_argument("--disable-gpu")   # для совместимости
-# options.add_argument("--disable-blink-features=AutomationControlled")
-# options.add_argument("--disable-notifications")
-# options.add_argument("--lang=en-US")
-# options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
-# options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-notifications")
+options.add_argument("--lang=en-US")
+options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+options.add_argument("--remote-debugging-port=9222")
 
 from config import VERSION_MAIN
 driver = uc.Chrome(options=options, version_main=VERSION_MAIN)
