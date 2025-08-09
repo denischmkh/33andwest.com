4ea2194ad366fc744b979dbfdf018e822ab2fff0
denischmkh <avraam.l2004@gmail.com>
2025-08-09 16:12:16 +0300

    Update parsers

--- modules/142_thekurlandagency.com.py
+++ modules/142_thekurlandagency.com.py
@@ -73,7 +73,6 @@ if response.status_code == 200:
     Artist.objects.filter(artist_name__in=missing_names, date_removed__isnull=True).update(date_removed=today)
 
     print(f"🟢 Synchronization complete. New: {len(current_names - existing_names)}, Missing: {len(missing_names)}")
-    print(current_names)
 
 else:
     raise Exception(f'Response error: {response.status_code} - {response.reason}')
