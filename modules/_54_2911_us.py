9bc97c4e6f8222a3a695b2e0983d3226bb624236
denischmkh <avraam.l2004@gmail.com>
2025-08-09 16:31:36 +0300

    Update parsers

--- modules/_54_2911_us.py
+++ modules/_54_2911_us.py
@@ -13,8 +13,6 @@ from parser_app.models import Artist
 today = timezone.now().date()
 
 headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "ru,en-US;q=0.9,en;q=0.8,ja;q=0.7",
     "cache-control": "no-cache",
     "pragma": "no-cache",
