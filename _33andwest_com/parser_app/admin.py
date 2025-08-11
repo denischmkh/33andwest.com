c2c39e8683876bf9d6fbfe82d3999b7f4486fd2f
denischmkh <avraam.l2004@gmail.com>
2025-08-11 10:42:53 +0300

    fix

--- _33andwest_com/parser_app/admin.py
+++ _33andwest_com/parser_app/admin.py
@@ -66,7 +66,7 @@ class ArtistAdmin(admin.ModelAdmin):
 admin.site.register(Artist, ArtistAdmin)
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ['site', 'date', 'status']
+    list_display = ['site', 'date', "scraped", "new", 'deleted', 'status']
     ordering = ['site']
 
 admin.site.register(Status, StatusAdmin)
