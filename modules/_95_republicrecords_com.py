39f1218ab38bd67721d026924a875c99a49ef553
denischmkh <avraam.l2004@gmail.com>
2025-08-11 12:48:18 +0300

    fix

--- modules/_95_republicrecords_com.py
+++ modules/_95_republicrecords_com.py
@@ -8,8 +8,6 @@ from parser_app.models import Artist
 today = timezone.now().date()
 
 headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "ru,en-US;q=0.9,en;q=0.8,ja;q=0.7",
     "cache-control": "no-cache",
     "pragma": "no-cache",
@@ -48,7 +46,6 @@ def parse95():
     response = requests.get(url, headers=headers, cookies=cookies)
 
     if response.status_code == 200:
-        print(response.text)
         soup = BeautifulSoup(response.text, "html.parser")
         artists = soup.find_all("p",class_="text_body_lg")
 
